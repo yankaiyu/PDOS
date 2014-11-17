f4c454e1f882fb1dc87418569fca85b3115c4aa7
yankaiyu <yankaiyu90@gmail.com>
2014-11-16 22:10:41 -0500

    First commit

---
+++ Parallel Degree of Separation/Parallel Degree of Separation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Parallel Degree of Separation
+//
+//  Created by Cary on 11/16/14.
+//  Copyright (c) 2014 Cary. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
